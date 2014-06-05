42cf9884c2b65c9a8cc0841527b367590d1e072d
lucas8 <luc.linux@mailoo.org>
2014-06-05 17:40:44 +0200

    Now using xinerama to detect screens.

--- server/screen.c
+++ server/screen.c
@@ -1,5 +1,6 @@
 
 #include "screen.h"
+#include <xcb/xinerama.h>
 #include <stdlib.h>
 
 srv_screen_t* load_screens(xcb_connection_t* c)
@@ -7,17 +8,23 @@ srv_screen_t* load_screens(xcb_connection_t* c)
     srv_screen_t* scr;
     srv_screen_t* act;
     srv_screen_t* first = NULL;
-    xcb_screen_t* xcbscr;
-    xcb_screen_iterator_t it;
+    xcb_screen_t* xcbscr = xcb_setup_roots_iterator(xcb_get_setup(c)).data;
+    xcb_xinerama_screen_info_t scrinfo;
+    xcb_xinerama_screen_info_iterator_t it;
+    xcb_xinerama_query_screens_reply_t* reply;
+    xcb_xinerama_query_screens_cookie_t cookie;
 
-    it = xcb_setup_roots_iterator(xcb_get_setup(c));
-    for(; it.rem; xcb_screen_next(&it)) {
-        xcbscr = it.data;
+    cookie = xcb_xinerama_query_screens(c);
+    reply  = xcb_xinerama_query_screens_reply(c, cookie, NULL);
+    it     = xcb_xinerama_query_screens_screen_info_iterator(reply);
+    free(reply);
+
+    for(; it.rem; xcb_xinerama_screen_info_next(&it)) {
         act = malloc(sizeof(srv_screen_t));
-        act->x = 0;
-        act->y = 0;
-        act->w = xcbscr->width_in_pixels;
-        act->h = xcbscr->height_in_pixels;
+        act->x = it.data->x_org;
+        act->y = it.data->y_org;
+        act->w = it.data->width;
+        act->h = it.data->height;
         act->xcbscr = xcbscr;
         act->next = NULL;
 
