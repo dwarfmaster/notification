0e8f994b4459993688918c42618cd536647cbd26
lucas8 <luc.linux@mailoo.org>
2014-06-09 17:57:40 +0200

    Removed call to dump_to_stdout in main.

--- server/main.c
+++ server/main.c
@@ -31,8 +31,6 @@ int main(int argc, char *argv[])
         fprintf(stderr, "Couldn't load config.\n");
         return 1;
     }
-    else
-        dump_to_stdout();
 
     /* Opening the connection. */
     c = xcb_connect(NULL, NULL);
