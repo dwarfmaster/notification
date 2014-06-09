919cd8df40a60de6469a1666feb2cc9c7cf948f6
lucas8 <luc.linux@mailoo.org>
2014-06-09 18:10:07 +0200

    Don't fail if a fifo already exists.

--- server/fifo.c
+++ server/fifo.c
@@ -14,12 +14,18 @@ static const char* _fifo_path = "/tmp/notifier.fifo";
 
 int init_fifo()
 {
+    struct stat buffer;
+
     if(_fifo >= 0)
         return 1;
 
     if(has_entry("global.fifo"))
         _fifo_path = get_string("global.fifo");
-    if(mkfifo(_fifo_path, 0777) != 0)
+    if(stat(_fifo_path, &buffer) < 0) {
+        if(mkfifo(_fifo_path, 0777) != 0)
+            return 0;
+    }
+    else if(!S_ISFIFO(buffer.st_mode))
         return 0;
 
     _fifo = open(_fifo_path, O_RDWR | O_NONBLOCK);
